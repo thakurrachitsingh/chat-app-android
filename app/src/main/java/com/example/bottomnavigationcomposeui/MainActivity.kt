aae88d4333e7be48a7150c8596764f72f3e5978b
Rachit Singh <rachit.singh@caastle.com>
2025-04-27 21:02:13 +0530

    27 April

--- app/src/main/java/com/example/bottomnavigationcomposeui/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationcomposeui/MainActivity.kt
@@ -45,19 +45,25 @@ import com.example.bottomnavigationcomposeui.pages.more.More
 import com.example.bottomnavigationcomposeui.pages.mycloset.MyCloset
 import com.example.bottomnavigationcomposeui.pages.referafriend.ReferAFriend
 import com.example.bottomnavigationcomposeui.pages.splash.SplashScreen
+import com.example.bottomnavigationcomposeui.pages.splash.StartViewModel
 import com.example.bottomnavigationcomposeui.ui.theme.BottomNavigationComposeUITheme
 import com.example.bottomnavigationcomposeui.utils.Screens
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.conflate
@@ -65,10 +71,15 @@ import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.retryWhen
 import kotlinx.coroutines.flow.sample
 import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 class MainActivity : ComponentActivity() {
     lateinit var job1: Deferred<Unit>
@@ -76,10 +87,15 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS),0)
         val flow1 = flowOf(1, 2, 3, 4, 5).onEach { delay(600) }
         val flow2 = flowOf("a", "b", "c", "d", "e").onEach { delay(800) }
         val state = MutableStateFlow("")
+
+
+
+
 //        val parentJob = CoroutineScope(Dispatchers.IO).launch{
 //            job1 = async{ flow1.collectLatest {
 //                Log.d("mainActivity1", it.toString())
@@ -112,36 +128,90 @@ class MainActivity : ComponentActivity() {
 //            }
 //        }
 
-        runBlocking {
-            delay(3000)
-            Log.d("mainActivity", "running blocking")
-        }
-        Log.d("mainActivity", "after blocking")
+
+//
+//        runBlocking {
+//            delay(5000)
+//            Log.d("mainActivity", "running blocking ${currentCoroutineContext()}")
+//        }
+//        Log.d("mainActivity", "after blocking")
+
+
 //        GlobalScope.launch {
-//            flow<Int>{
+//            flow{
 //                for(i in 1..5){
 //                    delay(200)
 //                    emit(i)
 //                }
-//            }
-//                .conflate()
-//                .collect {
-//                delay(500)
-//                Log.d("flow", "collectLatest: $it")
+//            }.retryWhen {cause, attempt ->
+//                 attempt<3
+////                .conflate()
+////                .collect {
+////                delay(500)
+////                Log.d("flow", "collectLatest: $it")
 //            }
 //        }
 
+        val handler = CoroutineExceptionHandler{ _, expection ->
+            Log.d("mainActivity", "error: ${expection.message}")
+        }
+        val superVisorJob = SupervisorJob()
+        val flow = MutableSharedFlow<Int>(5)
+        val floww = flow.asSharedFlow()
 
-    GlobalScope.launch {
-//        flow1
-//            .zip(flow2){a, b ->
-//                "$a  $b,"
-//            }
-//
-//            .collect{
-//            Log.d("flow", "collect: $it")
+
+//        GlobalScope.launch {
+//            flow.emit(1)
+//            delay(1000)
+//            flow.emit(2)
+//            delay(1000)
+//            flow.emit(3)
+//            delay(1000)
+//            flow.emit(4)
+//            delay(800)
+//            flow.emit(5)
 //        }
+
+        val supervisor = GlobalScope.launch(Dispatchers.IO + superVisorJob) {
+//            delay(2000)
+//            flow.collect{
+//                Log.d("mainActivity", "collect: $it")
+//            }
+
+//        supervisorScope {
+//            val job1 = async{
+//                delay(200)
+//                Log.d("mainActivity", "job1 done")
+//            }
+//            val job2 = async(handler){
+//                delay(800)
+////                throw Exception("error")
+//                Log.d("mainActivity", "job2 done")
+//            }
+//            val job3 = async{
+//                delay(1000)
+//                Log.d("mainActivity", "job3 done")
+//            }
+//            Log.d("mainActivity", "supervisor done")
+//        }
+
+        //withTimeout
+//        try {
+//            val response = withTimeout(2000){
+//                delay(2900)
+//                "answer"
+//            }
+//        }catch (e: Exception){
+//            Log.d("mainActivity", "error:${e}")
+//        }
+//        Log.d("mainActivity", "response:")
+////        Log.d("mainActivity", "response: $response")
     }
+
+//        GlobalScope.launch {
+//            delay(600)
+//            supervisor.cancel()
+//        }
         enableEdgeToEdge()
         setContent {
             BottomNavigationComposeUITheme {
@@ -168,3 +238,11 @@ class MainActivity : ComponentActivity() {
 //        Greeting("Android")
 //    }
 //}
+
+suspend fun extra(){
+    withContext(Dispatchers.IO){
+        delay(1000)
+        Log.d("mainActivity", "extra: $coroutineContext")
+    }
+}
+
